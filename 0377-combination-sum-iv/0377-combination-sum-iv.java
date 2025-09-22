349556a95990ed83662586c136eb1fc568594765
Abhishek Kr Singh <146166418+Abhishek-singh-007@users.noreply.github.com>
2025-09-22 23:04:11 +0530

    Time: 0 ms (100%), Space: 40.8 MB (90.85%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int solve(int[]nums, int target, int[] dp){
+    private int solve(int[] nums, int target,int[]dp){
         if(target == 0)return 1;
-        if(dp[target] != -1)return dp[target];
+        if(dp[target] != -1) return dp[target];
         int count=0;
         for(int i=0; i<nums.length; i++){
             if(target-nums[i]>=0){
-                count += solve(nums,target-nums[i], dp);
+                count += solve(nums,target-nums[i],dp);
             }
         }
         return dp[target]=count;
@@ -14,6 +14,6 @@ class Solution {
         int n=nums.length;
         int[] dp=new int[target+1];
         Arrays.fill(dp,-1);
-        return solve(nums, target, dp);
+        return solve(nums,target,dp);
     }
 }
