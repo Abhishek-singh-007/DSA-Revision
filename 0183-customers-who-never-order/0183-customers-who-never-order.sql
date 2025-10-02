d5467b9dc68d3671e6362f36d0cbb4395eb44995
Abhishek Kr Singh <146166418+Abhishek-singh-007@users.noreply.github.com>
2025-10-02 11:23:21 +0530

    Time: 1071 ms (5%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,5 +1,4 @@
-SELECT c.name AS Customers
-FROM Customers c
-LEFT JOIN Orders o
-    ON c.id = o.customerId
-WHERE o.id IS NULL;
+select c.name as customers from Customers c
+left join Orders o
+on c.id=o.customerId
+where o.id is null
