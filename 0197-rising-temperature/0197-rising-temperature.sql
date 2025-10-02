b70b29db899c54a8a066d23d1221ad6557fa4a12
Abhishek Kr Singh <146166418+Abhishek-singh-007@users.noreply.github.com>
2025-10-02 11:48:47 +0530

    Time: 632 ms (11.68%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,4 @@
-select a1.id 
-from Weather a1
+select a1.id from Weather a1
 join Weather a2
-on datediff(a1.recordDate, a2.recordDate)=1
+on datediff(a1.recordDate,a2.recordDate)=1
 where a1.temperature>a2.temperature
