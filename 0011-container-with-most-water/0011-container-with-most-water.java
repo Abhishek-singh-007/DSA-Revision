2005917496a7f28365d3b7f628d1196b5fbf2e2b
Abhishek Kr Singh <146166418+Abhishek-singh-007@users.noreply.github.com>
2026-07-15 13:31:27 +0530

    Time: 5 ms (82.64%), Space: 77.1 MB (93.25%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,20 +1,18 @@
 class Solution {
     public int maxArea(int[] arr) {
-        //[1 8 6 2 5 4 8 3 7]
-        int n = arr.length;
-        int left = 0;
-        int right = n-1;
-        int mxHeight = 0;
-        while(left < right){
-            // jo choota hoga uska length * right-left which is width
-            int currArea = Math.min(arr[left],arr[right]) * (right-left);
-            mxHeight = Math.max(mxHeight, currArea);
-            if(arr[left] < arr[right]){
+        int n=arr.length;
+        int left=0;
+        int right=n-1;
+        int mxheight=0;
+        while(left<right){
+            int currheight=Math.min(arr[left],arr[right])*(right-left);
+            mxheight=Math.max(mxheight,currheight);
+            if(arr[left]<arr[right]){
                 left++;
             }else{
                 right--;
             }
         }
-        return mxHeight;
+        return mxheight;
     }
 }
