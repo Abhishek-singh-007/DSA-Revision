1cc9fabe2b6f7d54281b6d88a51bc6079deacce7
Abhishek Kr Singh <146166418+Abhishek-singh-007@users.noreply.github.com>
2026-07-25 10:14:58 +0530

    Time: 1 ms (100%), Space: 42.7 MB (58.74%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,22 +1,20 @@
 class Solution {
     public int maxProduct(int n) {
-        char[] ch = String.valueOf(n).toCharArray();
-        int[] arr = new int[ch.length];
+        int max1 = 0, max2 = 0;
 
-        for (int i = 0; i < ch.length; i++) {
-            arr[i] = ch[i] - '0';
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit >= max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
+            }
+
+            n /= 10;
         }
-        Arrays.sort(arr);
-        int left = 0;
-int right = arr.length - 1;
 
-while (left < right) {
-    int temp = arr[left];
-    arr[left] = arr[right];
-    arr[right] = temp;
-    left++;
-    right--;
-}
-return arr[0]*arr[1];
+        return max1 * max2;
     }
 }
